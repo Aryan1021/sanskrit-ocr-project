aa3bc80f55f408483d4b56c60e3671e3c77ea76c
Aryan Raj <165568975+Aryan1021@users.noreply.github.com>
2026-03-12 19:17:15 +0530

    Setup Python virtual environment and install project dependencies

---
+++ src/test_env.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+
+print("TensorFlow Version:", tf.__version__)
+print("OpenCV Version:", cv2.__version__)
+
+print("Environment working successfully")
